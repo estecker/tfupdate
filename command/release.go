8b99095617b2556fb850659b25cc15d07ecf9307
Eddie Stecker <eddie.stecker@flyrlabs.com>
2024-07-02 22:14:33 -0700

    Bunch of GoLand suggested changes

--- command/release.go
+++ command/release.go
@@ -1,32 +1,32 @@
 package command
 
 import (
-	"strings"
+    "strings"
 
-	"github.com/mitchellh/cli"
+    "github.com/mitchellh/cli"
 )
 
 // ReleaseCommand is a command which just shows help for subcommands.
 type ReleaseCommand struct {
-	Meta
+    Meta
 }
 
 // Run runs the procedure of this command.
-func (c *ReleaseCommand) Run(args []string) int { // nolint revive unused-parameter
-	return cli.RunResultHelp
+func (c *ReleaseCommand) Run(_ []string) int { // nolint revive unused-parameter
+    return cli.RunResultHelp
 }
 
 // Help returns long-form help text.
 func (c *ReleaseCommand) Help() string {
-	helpText := `
+    helpText := `
 Usage: tfupdate release <subcommand> [options] [args]
 
   This command has subcommands for release version information.
 `
-	return strings.TrimSpace(helpText)
+    return strings.TrimSpace(helpText)
 }
 
 // Synopsis returns one-line help text.
 func (c *ReleaseCommand) Synopsis() string {
-	return "Get release version information"
+    return "Get release version information"
 }
--- tfupdate/context_test.go
+++ tfupdate/context_test.go
@@ -1,24 +1,24 @@
 package tfupdate
 
 import (
-	"os"
-	"path/filepath"
-	"testing"
+    "os"
+    "path/filepath"
+    "testing"
 
-	"github.com/davecgh/go-spew/spew"
-	"github.com/google/go-cmp/cmp"
-	"github.com/spf13/afero"
+    "github.com/davecgh/go-spew/spew"
+    "github.com/google/go-cmp/cmp"
+    "github.com/spf13/afero"
 )
 
 func TestModuleContextSelecetedProviders(t *testing.T) {
-	cases := []struct {
-		desc string
-		src  string
-		want []SelectedProvider
-	}{
-		{
-			desc: "simple",
-			src: `
+    cases := []struct {
+        desc string
+        src  string
+        want []SelectedProvider
+    }{
+        {
+            desc: "simple",
+            src: `
 terraform {
   required_version = "1.5.0"
 
@@ -40,24 +40,24 @@ terraform {
   }
 }
 `,
-			want: []SelectedProvider{
-				SelectedProvider{Source: "hashicorp/aws", Version: "5.4.0"},
-				SelectedProvider{Source: "hashicorp/null", Version: "3.1.1"},
-				SelectedProvider{Source: "integrations/github", Version: "4.28.0"},
-			},
-		},
-		{
-			desc: "empty",
-			src: `
+            want: []SelectedProvider{
+                {Source: "hashicorp/aws", Version: "5.4.0"},
+                {Source: "hashicorp/null", Version: "3.1.1"},
+                {Source: "integrations/github", Version: "4.28.0"},
+            },
+        },
+        {
+            desc: "empty",
+            src: `
 terraform {
   required_version = "1.5.0"
 }
 `,
-			want: []SelectedProvider{},
-		},
-		{
-			desc: "unknown source",
-			src: `
+            want: []SelectedProvider{},
+        },
+        {
+            desc: "unknown source",
+            src: `
 terraform {
   required_providers {
     aws = {
@@ -71,13 +71,13 @@ terraform {
   }
 }
 `,
-			want: []SelectedProvider{
-				SelectedProvider{Source: "hashicorp/aws", Version: "5.4.0"},
-			},
-		},
-		{
-			desc: "unknown version",
-			src: `
+            want: []SelectedProvider{
+                {Source: "hashicorp/aws", Version: "5.4.0"},
+            },
+        },
+        {
+            desc: "unknown version",
+            src: `
 terraform {
   required_providers {
     aws = {
@@ -91,13 +91,13 @@ terraform {
   }
 }
 `,
-			want: []SelectedProvider{
-				SelectedProvider{Source: "hashicorp/aws", Version: "5.4.0"},
-			},
-		},
-		{
-			desc: "provider block (unknown source)",
-			src: `
+            want: []SelectedProvider{
+                {Source: "hashicorp/aws", Version: "5.4.0"},
+            },
+        },
+        {
+            desc: "provider block (unknown source)",
+            src: `
 terraform {
   required_providers {
     aws = {
@@ -111,13 +111,13 @@ provider "null" {
   version = "3.2.1"
 }
 `,
-			want: []SelectedProvider{
-				SelectedProvider{Source: "hashicorp/aws", Version: "5.4.0"},
-			},
-		},
-		{
-			desc: "version constraint",
-			src: `
+            want: []SelectedProvider{
+                {Source: "hashicorp/aws", Version: "5.4.0"},
+            },
+        },
+        {
+            desc: "version constraint",
+            src: `
 terraform {
   required_providers {
     aws = {
@@ -132,101 +132,101 @@ terraform {
   }
 }
 `,
-			want: []SelectedProvider{
-				SelectedProvider{Source: "hashicorp/aws", Version: "5.4.0"},
-			},
-		},
-	}
+            want: []SelectedProvider{
+                {Source: "hashicorp/aws", Version: "5.4.0"},
+            },
+        },
+    }
 
-	for _, tc := range cases {
-		t.Run(tc.desc, func(t *testing.T) {
-			fs := afero.NewMemMapFs()
-			dirname := "test"
-			err := fs.MkdirAll(dirname, os.ModePerm)
-			if err != nil {
-				t.Fatalf("failed to create dir: %s", err)
-			}
-			err = afero.WriteFile(fs, filepath.Join(dirname, "main.tf"), []byte(tc.src), 0644)
-			if err != nil {
-				t.Fatalf("failed to write file: %s", err)
-			}
+    for _, tc := range cases {
+        t.Run(tc.desc, func(t *testing.T) {
+            fs := afero.NewMemMapFs()
+            dirname := "test"
+            err := fs.MkdirAll(dirname, os.ModePerm)
+            if err != nil {
+                t.Fatalf("failed to create dir: %s", err)
+            }
+            err = afero.WriteFile(fs, filepath.Join(dirname, "main.tf"), []byte(tc.src), 0644)
+            if err != nil {
+                t.Fatalf("failed to write file: %s", err)
+            }
 
-			gc := &GlobalContext{
-				fs: fs,
-			}
-			mc, err := NewModuleContext(dirname, gc)
-			if err != nil {
-				t.Fatalf("failed to new ModuleContext: %s", err)
-			}
+            gc := &GlobalContext{
+                fs: fs,
+            }
+            mc, err := NewModuleContext(dirname, gc)
+            if err != nil {
+                t.Fatalf("failed to new ModuleContext: %s", err)
+            }
 
-			got := mc.SelecetedProviders()
+            got := mc.SelectedProviders()
 
-			if diff := cmp.Diff(got, tc.want); diff != "" {
-				t.Errorf("got: %s, want = %s, diff = %s", spew.Sdump(got), spew.Sdump(tc.want), diff)
-			}
-		})
-	}
+            if diff := cmp.Diff(got, tc.want); diff != "" {
+                t.Errorf("got: %s, want = %s, diff = %s", spew.Sdump(got), spew.Sdump(tc.want), diff)
+            }
+        })
+    }
 }
 
 func TestSelectVersion(t *testing.T) {
-	cases := []struct {
-		desc        string
-		constraints []string
-		want        string
-	}{
-		{
-			desc:        "simple",
-			constraints: []string{"3.2.1"},
-			want:        "3.2.1",
-		},
-		{
-			desc:        "empty list",
-			constraints: []string{},
-			want:        "",
-		},
-		{
-			desc:        "empty string",
-			constraints: []string{""},
-			want:        "",
-		},
-		{
-			desc:        "return first one found",
-			constraints: []string{"1.2.3", "3.2.1"},
-			want:        "1.2.3",
-		},
-		{
-			desc:        "ignore parse error",
-			constraints: []string{"> 1.2.3"},
-			want:        "",
-		},
-		{
-			desc:        "ignore parse error and return first one found",
-			constraints: []string{"= 1.2.3", "3.2.1"},
-			want:        "3.2.1",
-		},
-	}
+    cases := []struct {
+        desc        string
+        constraints []string
+        want        string
+    }{
+        {
+            desc:        "simple",
+            constraints: []string{"3.2.1"},
+            want:        "3.2.1",
+        },
+        {
+            desc:        "empty list",
+            constraints: []string{},
+            want:        "",
+        },
+        {
+            desc:        "empty string",
+            constraints: []string{""},
+            want:        "",
+        },
+        {
+            desc:        "return first one found",
+            constraints: []string{"1.2.3", "3.2.1"},
+            want:        "1.2.3",
+        },
+        {
+            desc:        "ignore parse error",
+            constraints: []string{"> 1.2.3"},
+            want:        "",
+        },
+        {
+            desc:        "ignore parse error and return first one found",
+            constraints: []string{"= 1.2.3", "3.2.1"},
+            want:        "3.2.1",
+        },
+    }
 
-	for _, tc := range cases {
-		t.Run(tc.desc, func(t *testing.T) {
-			got := selectVersion(tc.constraints)
+    for _, tc := range cases {
+        t.Run(tc.desc, func(t *testing.T) {
+            got := selectVersion(tc.constraints)
 
-			if got != tc.want {
-				t.Errorf("got=%s, but want=%s", got, tc.want)
-			}
-		})
-	}
+            if got != tc.want {
+                t.Errorf("got=%s, but want=%s", got, tc.want)
+            }
+        })
+    }
 }
 
 func TestModuleContextResolveProviderShortNameFromSource(t *testing.T) {
-	cases := []struct {
-		desc   string
-		src    string
-		source string
-		want   string
-	}{
-		{
-			desc: "simple",
-			src: `
+    cases := []struct {
+        desc   string
+        src    string
+        source string
+        want   string
+    }{
+        {
+            desc: "simple",
+            src: `
 terraform {
   required_providers {
     github = {
@@ -236,12 +236,12 @@ terraform {
   }
 }
 `,
-			source: "integrations/github",
-			want:   "github",
-		},
-		{
-			desc: "multiple forks",
-			src: `
+            source: "integrations/github",
+            want:   "github",
+        },
+        {
+            desc: "multiple forks",
+            src: `
 terraform {
   required_providers {
     petoju = {
@@ -256,12 +256,12 @@ terraform {
   }
 }
 `,
-			source: "winebarrel/mysql",
-			want:   "winebarrel",
-		},
-		{
-			desc: "not found",
-			src: `
+            source: "winebarrel/mysql",
+            want:   "winebarrel",
+        },
+        {
+            desc: "not found",
+            src: `
 terraform {
   required_providers {
     petoju = {
@@ -276,37 +276,37 @@ terraform {
   }
 }
 `,
-			source: "foo/mysql",
-			want:   "",
-		},
-	}
+            source: "foo/mysql",
+            want:   "",
+        },
+    }
 
-	for _, tc := range cases {
-		t.Run(tc.desc, func(t *testing.T) {
-			fs := afero.NewMemMapFs()
-			dirname := "test"
-			err := fs.MkdirAll(dirname, os.ModePerm)
-			if err != nil {
-				t.Fatalf("failed to create dir: %s", err)
-			}
-			err = afero.WriteFile(fs, filepath.Join(dirname, "main.tf"), []byte(tc.src), 0644)
-			if err != nil {
-				t.Fatalf("failed to write file: %s", err)
-			}
+    for _, tc := range cases {
+        t.Run(tc.desc, func(t *testing.T) {
+            fs := afero.NewMemMapFs()
+            dirname := "test"
+            err := fs.MkdirAll(dirname, os.ModePerm)
+            if err != nil {
+                t.Fatalf("failed to create dir: %s", err)
+            }
+            err = afero.WriteFile(fs, filepath.Join(dirname, "main.tf"), []byte(tc.src), 0644)
+            if err != nil {
+                t.Fatalf("failed to write file: %s", err)
+            }
 
-			gc := &GlobalContext{
-				fs: fs,
-			}
-			mc, err := NewModuleContext(dirname, gc)
-			if err != nil {
-				t.Fatalf("failed to new ModuleContext: %s", err)
-			}
+            gc := &GlobalContext{
+                fs: fs,
+            }
+            mc, err := NewModuleContext(dirname, gc)
+            if err != nil {
+                t.Fatalf("failed to new ModuleContext: %s", err)
+            }
 
-			got := mc.ResolveProviderShortNameFromSource(tc.source)
+            got := mc.ResolveProviderShortNameFromSource(tc.source)
 
-			if got != tc.want {
-				t.Errorf("got: %s, want = %s", got, tc.want)
-			}
-		})
-	}
+            if got != tc.want {
+                t.Errorf("got: %s, want = %s", got, tc.want)
+            }
+        })
+    }
 }
